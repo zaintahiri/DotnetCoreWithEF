87958cfbf92555ddc851c062cbe6192ec17d26db
zaintahiri <zain.tahiri940@gmail.com>
2024-12-29 22:46:24 +0500

    Custom helps, Installed some client side validation libraries. jquery-ajax-unobtrusive library too

---
+++ DotnetCoreWithEF/Helpers/BigTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DotnetCoreWithEF.Helpers
+{
+    [HtmlTargetElement("big")]
+    [HtmlTargetElement(Attributes ="big")]
+    public class BigTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "h3";
+            output.Attributes.RemoveAll("big");
+            output.Attributes.SetAttribute("class","h3");
+
+        }
+    }
+}
