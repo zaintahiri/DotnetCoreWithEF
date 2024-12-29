2f7ec97354ba98658061711b5551e225b8804186
zaintahiri <zain.tahiri940@gmail.com>
2024-12-29 22:08:01 +0500

    CustomEmailTagHelper added that will be used as tag where you want to use. it is basically tag helper

---
+++ DotnetCoreWithEF/Helpers/CustomEmailTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace DotnetCoreWithEF.Helpers
+{
+    public class CustomEmailTagHelper:TagHelper
+    {
+        public string Email{ get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            output.Attributes.SetAttribute("href",$"mailto:{Email}");
+            output.Content.SetContent("My-Email:"+Email);
+        }
+    }
+}
