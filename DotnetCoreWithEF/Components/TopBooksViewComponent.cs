dff03d0e677836a36225b3a8a622052fd049df4b
zaintahiri <zain.tahiri940@gmail.com>
2025-01-02 13:58:45 +0500

    Basic concept and usage of the viewcomponent used in book store application

---
+++ DotnetCoreWithEF/Components/TopBooksViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetCoreWithEF.Components
+{
+    public class TopBooksViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
